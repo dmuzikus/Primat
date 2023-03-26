86733dd96da25b1270dbdec2910b603d746575a0
dmuzikus <daniil.muzikus@gmail.com>
2023-03-26 19:38:10 +0300

    update

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def func(x):
     return math.sin(x) - math.log(x * x, math.e) - 1
 
 
-def save_tick(a, b, x1, x2, diff):
-    print(round(a, 4), round(b, 4), round(x1, 4), round(x2, 4), f"{round(diff, 4)}%")
+def save_tick(a, b, x1, x2, diff, methode=None):
+    print(round(a, 4), round(b, 4), round(x1, 4), round(x2, 4), f"{round(diff, 4)}%", methode if methode else '')
 
 
 def methode_dichotomy(eps=0.005):
@@ -94,9 +94,9 @@ def methode_fibonacci(eps):
     b = 24
     k = 1
     func_k = 2
-    f_n, n = search_fib_by_natural((b-a)/eps)
-    eps = ((b-a)/f_n)*1.3
-    if b-a <= eps:
+    f_n, n = search_fib_by_natural((b - a) / eps)
+    eps = ((b - a) / f_n) * 1.3
+    if b - a <= eps:
         return (a + b) / 2, k, func_k
     length = b - a
     x1 = a + (fibonacci(n - k - 1) / fibonacci(n - k + 1)) * (b - a)
@@ -130,11 +130,43 @@ def methode_fibonacci(eps):
     return (x1 + x2) / 2, k, func_k
 
 
+def parabolic_method(a, b, eps):
+    m = (a + b) / 2
+    f_a, f_m, f_b = func(a), func(m), func(b)
+    iteration = 0
+    calls = 3
+    length = a - b
+    while b - a > eps:
+        p = ((m - a) ** 2) * (f_m - f_b) - ((m - b) ** 2) * (f_m - f_a)
+        q = 2 * ((m - a) * (f_m - f_b) - (m - b) * (f_m - f_a))
+        u = m - p / q
+        f_u = func(u)
+        calls += 1
+        if m > u:
+            if f_m < f_u:
+                a, f_a = u, f_u
+            else:
+                b, f_b = m, f_m
+                m, f_m = u, f_u
+        else:
+            if f_m > f_u:
+                a, f_a = m, f_m
+                m, f_m = u, f_u
+            else:
+                b, f_b = u, f_u
+
+        iteration += 1
+        # save_tick(a, b, m, u, ((a - b) / length) * 100)
+
+    """print(f"[Share's price]: {func((a + b) / 2)}\n\r[Time]: {(a + b) / 2}\n\r[Iterations]: {iteration}"
+          f"\n\r[Func calculations]: {calls}")"""
+    return (a + b) / 2, iteration, calls
+
+
 """eps = [5*(10**i) for i in range(-5, 1)]
 for val in eps:
     value, k, fun_k = methode_gold(val)
     print(val, k, fun_k)"""
-
 
 """x = np.arange(21, 26.5, 0.01)
 y = np.sin(x) - np.log(x*x) - 1
@@ -149,8 +181,92 @@ plt.ylim([-9, 0])
 plt.title("График функции (Метод Фибоначчи)")
 plt.xlabel("Время, ч")
 plt.ylabel("Цена, $")
-plt.show()"""
-eps = [5*(10**i) for i in range(-5, 1)]
+plt.show()
+eps = [5 * (10 ** i) for i in range(-5, 1)]
 for val in eps:
     value, k, fun_k = methode_fibonacci(val)
+    print(val, k, fun_k)"""
+
+
+def brent_method(a, c, eps):
+    gr = (math.sqrt(5) - 1) / 2
+    x = w = v = a + gr * (c - a)
+    fm = fw = fv = func(x)
+    d = e = 0
+    u = float('+inf')
+    iteration = 0
+    calls = 1
+    length = a - c
+    while c - a > eps:
+        iteration += 1
+        g, e = e, d
+        if len({x, w, v}) == len({fm, fw, fv}) == 3:
+            p = ((x - w) ** 2) * (fm - fv) - ((x - v) ** 2) * (fm - fw)
+            q = 2 * ((x - w) * (fm - fv) - (x - v) * (fm - fw))
+            u = x - p / q
+        if a + eps <= u <= c - eps and 2 * abs(u - x) < g:
+            algo_type = 'spi'
+            d = abs(u - x)
+        else:
+            algo_type = 'gss'
+            if x < (c + a) * .5:
+                d = c - x
+                u = x + gr * d
+            else:
+                d = x - a
+                u = x - gr * d
+
+        if abs(u - x) < eps:
+            """print(f"[Share's price]: {func(x)}\n\r[Time]: {x}\n\r[Iterations]: {iteration}"
+                  f"\n\r[Func calculations]: {calls}")"""
+            return x, iteration, calls
+
+        fu = func(u)
+        calls += 1
+        if fu <= fm:
+            if u >= x:
+                a = x
+            else:
+                c = x
+            v, w, x = w, x, u
+            fv, fw, fm = fw, fm, fu
+
+        else:
+            if u >= x:
+                c = u
+            else:
+                a = u
+
+            if fu <= fw or w == x:
+                v, w = w, u
+                fv, fw = fw, fu
+            elif fu <= fv or v == x or v == w:
+                v = u
+                fv = fu
+        # save_tick(a, c, x, u, ((a - c) / length) * 100, algo_type)
+    """print(f"[Share's price]: {func(x)}\n\r[Time]: {x}\n\r[Iterations]: {iteration}"
+          f"\n\r[Func calculations]: {calls}")"""
+    return x, iteration, calls
+
+
+"""value = brent_method(21, 24, 0.005)[0]
+
+x = np.arange(21, 26.5, 0.01)
+y = np.sin(x) - np.log(x*x) - 1
+func_line = plt.plot(x, y, label='y = sin(x) - log(x * x, e) - 1', color='steelblue', linewidth = 3)
+point = plt.scatter(value, func(value), color='orange', s=40, marker='o', label='global min')
+plt.legend(shadow=True, fontsize=12)
+plt.plot(x, y)
+plt.grid(True)
+plt.minorticks_on()
+plt.xlim([20.5, 27])
+plt.ylim([-9, 0])
+plt.title("График функции (Метод Брента)")
+plt.xlabel("Время, ч")
+plt.ylabel("Цена, $")
+plt.show()"""
+
+eps = [5 * (10 ** i) for i in range(-5, 1)]
+for val in eps:
+    value, k, fun_k = brent_method(21, 24, val)
     print(val, k, fun_k)
